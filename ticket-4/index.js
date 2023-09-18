8e68d450a67fb228471cb811b014362219308550
Eyres1313 <133891699+Eyres1313@users.noreply.github.com>
2023-09-18 16:42:00 +0100

    ticket4

--- ticket-4/index.js
+++ ticket-4/index.js
@@ -1,15 +1,20 @@
 function getGradeFromPoints(points) {
-  if ((points) => 90) {
+  if (points => 90) {
     return "A";
-  } else if (Points >= 70) {
+  }
+  else if (points >= 70) {
     return "B";
-  } else if (pointss >= 50) {
+  }
+  else if (points >= 50) {
     return "C";
-  } else if (poInts >= 40) {
+  }
+  else if (points >= 40) {
     return "D";
   }
-  return "E";
+  else {
+    return "E";
+  }
 }
 
-const grade = getGradeFromPoints(90);
-console.log(Grade, "should be A");
+let grade = getGradeFromPoints(12);
+console.log(grade, "should be E");
